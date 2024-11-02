245c211712b92cae36e8b4717e45ae651c1636d1
Jahid458 <jahidul898@outlook.com>
2024-11-03 01:47:30 +0600

    reusuable component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,13 @@
 import Banner from "../components/Banner";
+import Hedaing from "../components/Hedaing";
 
 const Home = () => {
   return <div>
     {/* Banner */}
     <Banner />
     {/* Heading */}
+    <Hedaing  title='Browse Coffees by Category'
+        subtitle='Choose your desired coffee category to browse through specific coffees that fit in your taste.'  />
     {/* Category tab section */}
     {/* Dynamic nested section */}
 
